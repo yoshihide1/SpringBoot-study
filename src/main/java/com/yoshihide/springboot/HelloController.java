beaf47d9ebe6c72933cd5d0470842eb85514cbf0
yoshihide <aqua.sky.33333@gmail.com>
2020-10-19 21:28:39 +0900

    データの削除機能

--- src/main/java/com/yoshihide/springboot/HelloController.java
+++ src/main/java/com/yoshihide/springboot/HelloController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.yoshihide.springboot.repositories.MyDataRepository;
@@ -50,6 +51,22 @@ public class HelloController {
 	@Transactional(readOnly = false)
 	public ModelAndView update(@ModelAttribute MyData mydata, ModelAndView mav) {
 		repository.saveAndFlush(mydata);
+		return new ModelAndView("redirect:/");
+	}
+
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public ModelAndView delete(@PathVariable int id, ModelAndView mav) {
+		mav.setViewName("delete");
+		mav.addObject("title", "delete mydata.");
+		Optional<MyData> data = repository.findById((long) id);
+		mav.addObject("formModel", data.get());
+		return mav;
+	}
+
+	@RequestMapping(value = "/delete", method = RequestMethod.POST)
+	@Transactional(readOnly = false)
+	public ModelAndView remove(@RequestParam long id, ModelAndView mav) {
+		repository.deleteById(id);
 		return new ModelAndView("redirect:/");
 	}
 
