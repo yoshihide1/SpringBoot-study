d0497c4ad6fd3aa7edb8489c402dbbceb756a2e0
yoshihide <aqua.sky.33333@gmail.com>
2020-10-20 20:08:45 +0900

    ID, Name, Mailでの検索

--- src/main/java/com/yoshihide/springboot/MyDataDaoImpl.java
+++ src/main/java/com/yoshihide/springboot/MyDataDaoImpl.java
@@ -44,11 +44,19 @@ public class MyDataDaoImpl implements MyDataDao<MyData> {
 		return (List<MyData>) entityManager.createQuery("from MyData where name = " + name).getResultList();
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<MyData> find(String fstr) {
 		List<MyData> list = null;
-		String qstr = "from MyData where id = :fstr";
-		Query query = entityManager.createQuery(qstr).setParameter("fstr", Long.parseLong(fstr));
+		String qstr = "from MyData where id = :fid or name like :fname or mail like :fmail";
+		Long fid = 0L;
+		try {
+			fid = Long.parseLong(fstr);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+		Query query = entityManager.createQuery(qstr).setParameter("fid", fid).setParameter("fname", "%" + fstr + "%")
+				.setParameter("fmail", fstr + "@%");
 		list = query.getResultList();
 		return list;
 	}
