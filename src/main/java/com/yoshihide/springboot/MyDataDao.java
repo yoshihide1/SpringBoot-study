3f25fa8196d99f820abf806f99b3ae8a0a97f8a2
yoshihide <aqua.sky.33333@gmail.com>
2020-10-20 15:06:26 +0900

    データベースアクセス

---
+++ src/main/java/com/yoshihide/springboot/MyDataDao.java
@@ -0,0 +1,8 @@
+package com.yoshihide.springboot;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface MyDataDao<T> extends Serializable {
+	public List<T> getAll();
+}
--- src/main/java/com/yoshihide/springboot/PhoneValidator.java
+++ src/main/java/com/yoshihide/springboot/PhoneValidator.java
@@ -4,11 +4,23 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class PhoneValidator implements ConstraintValidator<Phone, String> {
+	private boolean onlyNumber = false;
 
 	@Override
-	public boolean isValid(String value, ConstraintValidatorContext context) {
-		// TODO 自動生成されたメソッド・スタブ
-		return false;
+	public void initialize(Phone phone) {
+		onlyNumber = phone.onlyNumber();
+	}
+
+	@Override
+	public boolean isValid(String input, ConstraintValidatorContext ctx) {
+		if (input == null) {
+			return false;
+		}
+		if (onlyNumber) {
+			return input.matches("[0-9]*");
+		} else {
+			return input.matches("[0-9()-]*");
+		}
 	}
 
 }
