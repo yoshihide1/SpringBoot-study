a263595008f0ad01f215201ab1a7bf0ca88cb0ab
yoshihide <aqua.sky.33333@gmail.com>
2020-10-21 21:14:20 +0900

    Beanクラスの作成

--- src/main/java/com/yoshihide/springboot/HelloController.java
+++ src/main/java/com/yoshihide/springboot/HelloController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,10 +18,23 @@ import com.yoshihide.springboot.repositories.MyDataRepository;
 public class HelloController {
 
 	@Autowired
+	MyDataBean myDataBean;
+
+	@Autowired
 	MyDataRepository repository;
 
 	@Autowired
 	private MyDataService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ModelAndView indexById(@PathVariable long id, ModelAndView mav) {
+		mav.setViewName("pickup");
+		mav.addObject("title", "Pickup Page");
+		String table = "<table>" + myDataBean.getTableTagById(id) + "</table>";
+		mav.addObject("msg", "pickup data id = " + id);
+		mav.addObject("data", table);
+		return mav;
+	}
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView index(ModelAndView mav) {
--- src/main/java/com/yoshihide/springboot/MyBootAppConfig.java
+++ src/main/java/com/yoshihide/springboot/MyBootAppConfig.java
@@ -1,5 +1,13 @@
 package com.yoshihide.springboot;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MyBootAppConfig {
 
+	@Bean
+	MyDataBean myDataBean() {
+		return new MyDataBean();
+	}
 }
