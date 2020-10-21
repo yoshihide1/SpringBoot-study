c8ead16f8bca528641c075c27904a870426f8c6a
yoshihide <64786947+yoshihide1@users.noreply.github.com>
2020-10-21 14:20:07 +0900

    Revert "6章部分"

--- src/main/java/com/yoshihide/springboot/MyData.java
+++ src/main/java/com/yoshihide/springboot/MyData.java
@@ -1,62 +1,30 @@
 package com.yoshihide.springboot;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
+//import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "mydata")
-@NamedQuery(name = "findWithName", query = "from MyData where name like :fname")
-@NamedQuery(name = "findByAge", query = "from MyData where age > :min and age < :max")
-
 public class MyData {
-
-	@OneToMany(cascade = CascadeType.ALL)
-	@Column(nullable = true)
-	private List<MsgData> msgdatas;
-
-	public List<MsgData> getMsgdatas() {
-		return msgdatas;
-	}
-
-	public void setMsgdatas(List<MsgData> msgdatas) {
-		this.msgdatas = msgdatas;
-	}
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column
-	@NotNull
 	private long id;
 
-	@Column(length = 50, nullable = false)
-	@NotEmpty
+	(length=50,nullable=false)
 	private String name;
 
-	@Column(length = 200, nullable = true)
-	@Email
+	(length=200,nullable=true)
 	private String mail;
 
-	@Column(nullable = true)
-	@Min(0)
-	@Max(200)
+	(nullable=true)
 	private Integer age;
 
-	@Column(nullable = true)
-	@Phone(onlyNumber = true)
+	(nullable=true)
 	private String memo;
 
 	public long getId() {
