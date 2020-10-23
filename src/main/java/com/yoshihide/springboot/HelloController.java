cecf9d68950e1c5bc5eb7c4b65e2285034d91600
yoshihide <aqua.sky.33333@gmail.com>
2020-10-23 13:42:01 +0900

    検索、DB保存、ページネーションをひとつに。

--- src/main/java/com/yoshihide/springboot/HelloController.java
+++ src/main/java/com/yoshihide/springboot/HelloController.java
@@ -1,29 +1,38 @@
 package com.yoshihide.springboot;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+//追加
+import org.springframework.data.domain.Pageable;
+//ここまで
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.yoshihide.springboot.repositories.MyDataMongoRepository;
+//import com.yoshihide.springboot.repositories.MyDataRepository;
 
 @Controller
 public class HelloController {
 
 	@Autowired
-	MyDataMongoRepository repository;
+	MyDataMongoRepository mongoRepository;
+
+//	@Autowired
+//	MyDataRepository myRepository;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView index(ModelAndView mav) {
+	public ModelAndView index(ModelAndView mav, /* 追加 */Pageable pageable) {
 		mav.setViewName("index");
 		mav.addObject("title", "Find Page");
 		mav.addObject("msg", "MyDataMongoのサンプル(/index)");
-		Iterable<MyDataMongo> list = repository.findAll();
+		Iterable<MyDataMongo> list = mongoRepository.findAll(pageable);
 		mav.addObject("datalist", list);
 		return mav;
 	}
@@ -32,7 +41,7 @@ public class HelloController {
 	@Transactional(readOnly = false)
 	public ModelAndView form(@RequestParam("name") String name, @RequestParam("memo") String memo, ModelAndView mav) {
 		MyDataMongo mydata = new MyDataMongo(name, memo);
-		repository.save(mydata);
+		mongoRepository.save(mydata);
 		return new ModelAndView("redirect:/");
 	}
 
@@ -42,7 +51,7 @@ public class HelloController {
 		mav.addObject("title", "Find Page");
 		mav.addObject("msg", "MyDataのサンプル");
 		mav.addObject("value", "");
-		List<MyDataMongo> list = repository.findAll();
+		List<MyDataMongo> list = mongoRepository.findAll();
 		mav.addObject("datalist", list);
 		return mav;
 	}
@@ -56,9 +65,18 @@ public class HelloController {
 			mav.addObject("title", "Find Result");
 			mav.addObject("msg", "「" + param + "」の検索結果");
 			mav.addObject("value", param);
-			List<MyDataMongo> list = repository.findByName(param);
+			List<MyDataMongo> list = mongoRepository.findByNameLike(param);
 			mav.addObject("datalist", list);
 		}
+		return mav;
+	}
+
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public ModelAndView delete(@PathVariable int id, ModelAndView mav) {
+		mav.setViewName("delete");
+		mav.addObject("title", "Delete Page");
+		Optional<MyDataMongo> data = mongoRepository.findById((long) id);
+		mav.addObject("formModel", data.get());
 		return mav;
 	}
 
--- src/main/java/com/yoshihide/springboot/repositories/MyDataMongoRepository.java
+++ src/main/java/com/yoshihide/springboot/repositories/MyDataMongoRepository.java
@@ -2,6 +2,10 @@ package com.yoshihide.springboot.repositories;
 
 import java.util.List;
 
+//追加
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+//ここまで
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +13,13 @@ import com.yoshihide.springboot.MyDataMongo;
 
 @Repository
 public interface MyDataMongoRepository extends MongoRepository<MyDataMongo, Long> {
-	public List<MyDataMongo> findByName(String s);
+
+	public List<MyDataMongo> findByNameLike(String s);
+
+	// 追加
+//	public Optional<MyDataMongo> findById(long id);
+
+	public Page<MyDataMongo> findAll(Pageable pageable);
+	// ここまで
+
 }
